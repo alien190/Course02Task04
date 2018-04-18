62047babe3f1589999a44a2aa25077ae0f60b707
alien1900 <alien1900@bk.ru>
2018-04-18 11:56:34 +0700

    state1

---
+++ app/src/main/java/com/example/ivanovnv/course02task04/RecycleFragment.java
@@ -0,0 +1,76 @@
+package com.example.ivanovnv.course02task04;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+/**
+ * Created by IvanovNV on 18.04.2018.
+ */
+
+public class RecycleFragment extends Fragment {
+
+    private ImageButton mButtonAddText;
+    private ImageButton mButtonAddImage;
+    private RecyclerView mRecycle;
+
+    public static RecycleFragment newInstance() {
+        return new RecycleFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fr_recycle, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        mButtonAddText = view.findViewById(R.id.bt_add_text);
+        mButtonAddImage = view.findViewById(R.id.bt_add_image);
+        mRecycle = view.findViewById(R.id.recycle);
+        mRecycle.setLayoutManager(new LinearLayoutManager(view.getContext()));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mButtonAddText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mButtonAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        mButtonAddText.setOnClickListener(null);
+        mButtonAddImage.setOnClickListener(null);
+
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        mButtonAddText = null;
+        mButtonAddImage = null;
+        mRecycle = null;
+
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/example/ivanovnv/course02task04/ViewHolderText.java
@@ -0,0 +1,18 @@
+package com.example.ivanovnv.course02task04;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by IvanovNV on 18.04.2018.
+ */
+
+public class ViewHolderText extends RecyclerView.ViewHolder {
+
+
+    public ViewHolderText(View itemView) {
+        super(itemView);
+
+
+    }
+}
